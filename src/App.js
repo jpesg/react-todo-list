535c6b9d2e74e00bca6f0794df1b2a50c3696d27
jpesg <jpesgil@gmail.com>
2019-04-20 12:02:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <TodoInput />
+          </div>
+          <div className="row">
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
